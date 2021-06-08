2b1729e24415b41f8c718273b4f4664758c0e2f4
youssef rafrafi <rafrafi.youssef@hotmail.com>
2021-06-08 17:43:20 +0200

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times { 
+    article = Article.new(title:Faker::Lorem.word , content: Faker::Lorem.sentence)
+    article.save
+}
